fcbd7bedd09e1d4c0989465da87275655f023485
Alan <a_saudabekov@kbtu.kz>
2023-09-24 08:54:38 +0600

    urls for views

--- backend/authorize/urls.py
+++ backend/authorize/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'authorize'
+
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='user-home'),
+    path('signin/', views.SignInUser.as_view(), name='user-signin'),
+    path('signup/', views.SignUpUser.as_view(), name='user-signup'),
+    path('signout/', views.signout_user, name='user-signout'),
 ]
