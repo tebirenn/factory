3e29b03c6a920f2ff1ad5919dd5b42e0fec88c19
Alan <a_saudabekov@kbtu.kz>
2023-09-25 04:21:17 +0600

    custom user created and registered

--- backend/authorize/admin.py
+++ backend/authorize/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'tg_id')
+# admin.site.register(User)
--- backend/authorize/models.py
+++ backend/authorize/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    tg_id = models.CharField(max_length=255, null=True, default=None)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='users',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='users',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+        error_messages={
+            'add': 'The permission you are trying to add already exists for the user.',
+            'remove': 'The permission you are trying to remove does not exist for the user.',
+        },
+    )
